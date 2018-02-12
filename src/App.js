8d0353c6284213d2683f4841e6e8e3b5e11033c0
Maciej001 <m.nowakowskipl@gmail.com>
2018-02-12 20:33:36 +0000

    Basic app structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+// App.js
+
 import React from "react";
+import Header from "./header/Header";
+import Main from "./main/Main";
 
 const App = () => {
   return (
     <div>
-      <h1>Sweet Pumpkins</h1>
+      <Header />
+      <Main />
     </div>
   );
 };
